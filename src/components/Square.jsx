a378c352128647183263e6749cb94135b84906a1
Rishika Gupta <itsrishikagupta@gmail.com>
2023-05-21 17:16:37 +0530

    installed saas .created board and square components barebones

---
+++ src/components/Square.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line react/prop-types
+const Square = ({value}) => {
+  // console.log(children);
+  // eslint-disable-next-line react/prop-types
+  return <button type="button" className="square">
+    {value}
+    </button>
+  
+  
+  
+  
+};
+
+export default Square;
